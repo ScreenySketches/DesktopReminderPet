15c98824353e2d7c0185724d9087609f97edabbc
Sean Dotson <seand0602@gmail.com>
2022-12-16 12:44:54 -0800

    Init
    Initialization of files

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TaskPet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.imageListIdleL = new System.Windows.Forms.ImageList(this.components);
+            this.timerAnimateSprite = new System.Windows.Forms.Timer(this.components);
+            this.imageListWalkingL = new System.Windows.Forms.ImageList(this.components);
+            this.imageListIdleR = new System.Windows.Forms.ImageList(this.components);
+            this.imageListWalkingR = new System.Windows.Forms.ImageList(this.components);
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.imageListDragL = new System.Windows.Forms.ImageList(this.components);
+            this.imageListDragR = new System.Windows.Forms.ImageList(this.components);
+            this.imageListFalling = new System.Windows.Forms.ImageList(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb
+            // 
+            this.pb.BackColor = System.Drawing.Color.Transparent;
+            this.pb.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb.Location = new System.Drawing.Point(0, 0);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(72, 93);
+            this.pb.TabIndex = 0;
+            this.pb.TabStop = false;
+            this.pb.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pb_MouseDown);
+            this.pb.MouseLeave += new System.EventHandler(this.pb_MouseLeave);
+            this.pb.MouseHover += new System.EventHandler(this.pb_Hover);
+            this.pb.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pb_MouseMove);
+            this.pb.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pb_MouseUp);
+            // 
+            // imageListIdleL
+            // 
+            this.imageListIdleL.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListIdleL.ImageStream")));
+            this.imageListIdleL.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListIdleL.Images.SetKeyName(0, "idle_l_0.png");
+            this.imageListIdleL.Images.SetKeyName(1, "idle_l_1.png");
+            this.imageListIdleL.Images.SetKeyName(2, "idle_l_2.png");
+            this.imageListIdleL.Images.SetKeyName(3, "idle_l_3.png");
+            // 
+            // timerAnimateSprite
+            // 
+            this.timerAnimateSprite.Enabled = true;
+            this.timerAnimateSprite.Interval = 245;
+            this.timerAnimateSprite.Tick += new System.EventHandler(this.timerAnimateSprite_Tick);
+            // 
+            // imageListWalkingL
+            // 
+            this.imageListWalkingL.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListWalkingL.ImageStream")));
+            this.imageListWalkingL.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListWalkingL.Images.SetKeyName(0, "walk_l_0.png");
+            this.imageListWalkingL.Images.SetKeyName(1, "walk_l_1.png");
+            this.imageListWalkingL.Images.SetKeyName(2, "walk_l_2.png");
+            this.imageListWalkingL.Images.SetKeyName(3, "walk_l_3.png");
+            // 
+            // imageListIdleR
+            // 
+            this.imageListIdleR.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListIdleR.ImageStream")));
+            this.imageListIdleR.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListIdleR.Images.SetKeyName(0, "idle_r_0.png");
+            this.imageListIdleR.Images.SetKeyName(1, "idle_r_1.png");
+            this.imageListIdleR.Images.SetKeyName(2, "idle_r_2.png");
+            this.imageListIdleR.Images.SetKeyName(3, "idle_r_3.png");
+            // 
+            // imageListWalkingR
+            // 
+            this.imageListWalkingR.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListWalkingR.ImageStream")));
+            this.imageListWalkingR.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListWalkingR.Images.SetKeyName(0, "walk_r_0.png");
+            this.imageListWalkingR.Images.SetKeyName(1, "walk_r_1.png");
+            this.imageListWalkingR.Images.SetKeyName(2, "walk_r_2.png");
+            this.imageListWalkingR.Images.SetKeyName(3, "walk_r_3.png");
+            // 
+            // UpdateTimer
+            // 
+            this.UpdateTimer.Enabled = true;
+            this.UpdateTimer.Interval = 25;
+            this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
+            // 
+            // imageListDragL
+            // 
+            this.imageListDragL.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListDragL.ImageStream")));
+            this.imageListDragL.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListDragL.Images.SetKeyName(0, "drag_l_0.png");
+            this.imageListDragL.Images.SetKeyName(1, "drag_l_1.png");
+            this.imageListDragL.Images.SetKeyName(2, "drag_l_2.png");
+            this.imageListDragL.Images.SetKeyName(3, "drag_l_3.png");
+            // 
+            // imageListDragR
+            // 
+            this.imageListDragR.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListDragR.ImageStream")));
+            this.imageListDragR.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListDragR.Images.SetKeyName(0, "drag_r_0.png");
+            this.imageListDragR.Images.SetKeyName(1, "drag_r_1.png");
+            this.imageListDragR.Images.SetKeyName(2, "drag_r_2.png");
+            this.imageListDragR.Images.SetKeyName(3, "drag_r_3.png");
+            // 
+            // imageListFalling
+            // 
+            this.imageListFalling.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListFalling.ImageStream")));
+            this.imageListFalling.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListFalling.Images.SetKeyName(0, "fall_l_0.png");
+            this.imageListFalling.Images.SetKeyName(1, "fall_l_1.png");
+            this.imageListFalling.Images.SetKeyName(2, "fall_l_2.png");
+            this.imageListFalling.Images.SetKeyName(3, "fall_l_3.png");
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(181, 48);           
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(72, 93);
+            this.Controls.Add(this.pb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public System.Windows.Forms.ImageList imageListIdleL;
+        public System.Windows.Forms.PictureBox pb;
+        public System.Windows.Forms.Timer timerAnimateSprite;
+        public System.Windows.Forms.ImageList imageListWalkingL;
+        public System.Windows.Forms.ImageList imageListIdleR;
+        public System.Windows.Forms.ImageList imageListWalkingR;
+        public System.Windows.Forms.Timer UpdateTimer;
+        public System.Windows.Forms.ImageList imageListDragL;
+        public System.Windows.Forms.ImageList imageListDragR;
+        public System.Windows.Forms.ImageList imageListFalling;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+namespace TaskPet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MovementDelay();
+            TurnDelay();
+            RectInitialize();
+            
+            
+
+        }
+
+        //variables
+        
+        int imageNumb = 0;
+        public bool isIdle = true;
+        public bool isWalking = false;
+        public bool isFacingLeft = true;
+        public bool isBeingDragged;
+        public bool isFalling = true;
+        int Force;
+        public int movementSpeed;
+        public int walkDuration;
+        public int idleDuration;
+        public int turnDuration;
+        Random randomGenerator = new Random();
+        Rectangle windowRectangle = new Rectangle();
+        Rectangle desktopRectangle = new Rectangle();
+        private Point lastLocation;
+
+
+
+
+
+
+        public async void TurnDelay()
+        {
+            
+            for(int i = 0; i < 3; i++)
+            {
+                while(isBeingDragged == false)
+                {
+                    turnDuration = randomGenerator.Next(3000, 13000);
+                    isFacingLeft = false;
+                    await Task.Delay(turnDuration);
+                    isFacingLeft = true;
+                    turnDuration = randomGenerator.Next(3000, 18000);
+                    await Task.Delay(turnDuration);
+                    
+                }
+                
+            }
+        }
+
+
+        
+
+
+
+        public async void MovementDelay()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                
+                idleDuration = randomGenerator.Next(2000, 10000);
+                walkDuration = randomGenerator.Next(2000, 4000);
+                movementSpeed = randomGenerator.Next(9, 15);
+
+                isIdle = true;
+                isWalking = false;
+                await Task.Delay(idleDuration);
+                isIdle = false;
+                isWalking = true;
+                await Task.Delay(walkDuration);
+                i = 0;
+            }           
+        }
+
+        public void RectInitialize()
+        {
+            windowRectangle.Size = Size;
+            windowRectangle.Location = Location;
+            desktopRectangle = Screen.GetBounds(windowRectangle);
+            
+        }
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void timerAnimateSprite_Tick(object sender, EventArgs e)
+        {
+            if (isFalling)
+            {
+                timerAnimateSprite.Interval = 100;
+
+                pb.Image = imageListFalling.Images[imageNumb];
+
+                if (imageNumb == imageListFalling.Images.Count - 1)
+                {
+                    imageNumb = 0;
+                }
+                else
+                {
+                    imageNumb++;
+                }
+
+            }else if (isBeingDragged)
+            {
+                timerAnimateSprite.Interval = 100;
+
+
+                if (isFacingLeft)
+                {
+                    pb.Image = imageListDragL.Images[imageNumb];
+
+                    if (imageNumb == imageListDragL.Images.Count - 1)
+                    {
+                        imageNumb = 0;
+                    }
+                    else
+                    {
+                        imageNumb++;
+                    }
+                }
+                else
+                {
+                    pb.Image = imageListDragR.Images[imageNumb];
+
+                    if (imageNumb == imageListDragR.Images.Count - 1)
+                    {
+                        imageNumb = 0;
+                    }
+                    else
+                    {
+                        imageNumb++;
+                    }
+                }
+
+
+            }
+            else if (isIdle)
+            {
+                timerAnimateSprite.Interval = 165;
+
+                if (isFacingLeft)
+                {
+                    pb.Image = imageListIdleL.Images[imageNumb];
+
+                    if (imageNumb == imageListIdleL.Images.Count - 1)
+                    {
+                        imageNumb = 0;
+                    }
+                    else
+                    {
+                        imageNumb++;
+                    }
+                }
+                else
+                {
+                    pb.Image = imageListIdleR.Images[imageNumb];
+
+                    if (imageNumb == imageListIdleR.Images.Count - 1)
+                    {
+                        imageNumb = 0;
+                    }
+                    else
+                    {
+                        imageNumb++;
+                    }
+                }
+
+
+            }
+            else if (isWalking)
+            {
+                timerAnimateSprite.Interval = 190 - (10 * movementSpeed);
+
+                if (isFacingLeft)
+                {
+                    pb.Image = imageListWalkingL.Images[imageNumb];
+
+                    if (imageNumb == imageListWalkingL.Images.Count - 1)
+                    {
+                        imageNumb = 0;
+                    }
+                    else
+                    {
+                        imageNumb++;
+                    }
+
+
+
+
+
+
+                }
+                else
+                {
+                    pb.Image = imageListWalkingR.Images[imageNumb];
+
+                    if (imageNumb == imageListWalkingR.Images.Count - 1)
+                    {
+                        imageNumb = 0;
+                    }
+                    else
+                    {
+                        imageNumb++;
+                    }
+
+
+
+                }
+
+            }
+
+
+
+
+        }
+
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            //     v Dragging Check v
+
+            
+
+            //     v Bounds Check v
+
+            windowRectangle.Location = Location;
+
+            //     v Movement v
+
+            if (isBeingDragged)
+            {
+
+            }
+            else if (!isFalling)
+            {
+                if (isWalking)
+                {
+                    if (isFacingLeft)
+                    {
+
+                        Left -= movementSpeed - 7;
+
+
+
+                    }
+                    else
+                    {
+                        Left += movementSpeed - 7;
+
+                    }
+
+                    // v Collision Detection v
+
+                    if (!desktopRectangle.Contains(windowRectangle))
+                    {
+                        if (windowRectangle.Left < desktopRectangle.Left)
+                        {
+                            isFacingLeft = false;
+                        }
+                        if (windowRectangle.Right > desktopRectangle.Right)
+                        {
+                            isFacingLeft = true;
+                        }
+                    }
+
+                }
+            }
+            if (isFalling == true)
+            {               
+                Top -= Force;
+                Force -= 1;
+            }
+            if(windowRectangle.Top + windowRectangle.Height >= desktopRectangle.Height)
+            {
+                Top = desktopRectangle.Height - windowRectangle.Height;
+                isFalling = false;
+                Force = 0;
+            }
+            else if(!isBeingDragged)
+            {
+                isFalling = true;
+                Top += 1;               
+            }
+        }       
+        private void pb_Hover(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void pb_MouseLeave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pb_MouseDown(object sender, MouseEventArgs e)
+        {
+            isBeingDragged = true;
+            lastLocation = e.Location;
+            if(e.Button == MouseButtons.Right)
+            {
+                contextMenu.Show(this, new Point(e.X + ((Control)sender).Left + 20, e.Y + ((Control)sender).Top + 20));
+            }
+            
+        }
+        private void pb_MouseUp(object sender, MouseEventArgs e)
+        {
+            isBeingDragged = false;
+            TurnDelay();
+        }
+
+        private void pb_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isBeingDragged)
+            {
+                Location = new Point(
+                    (Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                Update();
+            }
+        }       
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
